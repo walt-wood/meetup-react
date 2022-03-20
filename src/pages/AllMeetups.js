9478962539a5ccc777a6b33babc9bbf4f205d8dd
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-20 15:24:18 -0500

    Rendrd dummy_data in AllMeetups & added 2 files

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,10 +1,37 @@
 // Loads and displays all meetups
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
 
+// Can render an array of list items like this: {[<li>Item1</li>, <li>Item2</li>]}
+// React recommends each list item havae a unique key: <li key={meetup.id}>
 function AllMeetupsPage() {
   return (
-    <div>
-      <p>All Meetups Page</p>
-    </div>
+    <section>
+      <h1>All Meetups</h1>      
+      <ul>
+      {DUMMY_DATA.map((meetup) => {
+        return <li key={meetup.id}>{meetup.title}</li>;
+      })}
+      </ul>
+    </section>
   );
 }
 
