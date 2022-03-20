3636a25363bef3b4247d0b95c9f4bfb5383d840c
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-20 18:21:38 -0500

    Added navigation after form submission.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,8 +1,12 @@
 // Loads only new meetups
 
+// useHistory hook now is useNavigate
+import { useNavigate } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  //used to manipulate browser history
+  const navigate = useNavigate();
   // Sending http request to Firebase database.
   function addMeetupHandler(meetupData) {
     // fetch(); is stnd JS to send http get request. Adding meetups.json on the end tells Firebase to create a new collection named meetups.
@@ -15,7 +19,12 @@ function NewMeetupPage() {
           "Content-Type": "application.json",
         },
       }
-    );
+    ).then(() => {
+      // then retns promise from ?
+      // history.push() pushes to top of history stack
+      // replace won't allow usr to use back button
+      navigate("/meetup-react/", { replace: true });
+    });
   }
   return (
     <section>
