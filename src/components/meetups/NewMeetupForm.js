c0634374dc971f9efbbbe27b38a7a25bbd58b86f
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-20 17:03:50 -0500

    Saving

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,61 @@
 import classes from "./NewMeetupForm.module.css";
 import Card from "../ui/Card";
+// Allows use of React created ref objects
+import { useRef } from "react";
 
 function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    // Receives event object sent by React
+    // Prevents browser default of reloading when form submitted.
+    event.preventDefault();
+
+    // Current entered value of input tag
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    // Store entered values to memory
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    console.log(meetupData);
+  }
+
+  // For keeping track of input, there are two ways. We could add onChange event listener to each input tag which stores each letter typed, but
+  // we are going to use the data when the form is submitted.
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea required id="descriptioin" rows="5" />
+          <textarea
+            required
+            id="descriptioin"
+            rows="5"
+            ref={descriptionInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
