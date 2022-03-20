abd8f5faff70de501807088a797d576cb65b58a0
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-19 19:11:37 -0500

    Added npm install react-router-dom for routing.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Command: npm install react-router-dom, allows for routing inside the program and must be installed manually.
+// Routing allows us to change whats visible on the screen with client-side javascript to give the illusion of different pages.
+// Acutally, everything is handled on the index.html page.
+
 ReactDOM.render(<App />, document.getElementById("root"));
