d9c37e06107953958fe5c5d3a815ef13a9aadff4
wwood44 <80364037+wwood44@users.noreply.github.com>
2022-03-21 12:18:41 -0500

    Added code to get data from Firebase

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,34 +1,58 @@
 // Loads and displays all meetups
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+import { useState, useEffect } from "react";
 
 // Can render an array of list items like this: {[<li>Item1</li>, <li>Item2</li>]}
 // React recommends each list item havae a unique key: <li key={meetup.id}>
 function AllMeetupsPage() {
+  // Start in loading state
+  const [isLoading, setIsLoading] = useState(true);
+  // Array of meetups -  empty array is overrided once we get data
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  // Use effect allows running code based on conditions, the second argument is the conditions and if omitted.
+  // If array is empty, will only execute the first time. This happens b/c it checks the conditions against themselves based on the
+  // last time useEffect was executed. The code executes whenever dependency(second arg) changes. Should add all external values the
+  // code relies on. UseEffect is proper solution for executing side effects and when they should run.
+  useEffect(() => {
+    setIsLoading(true);
+    // Get data from database.
+    fetch(
+      "https://react-getting-started-9eab0-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        // converts json data to javascript object
+        return response.json();
+        // json() also returns promise, so another then is needed to reach data
+      })
+      .then((data) => {
+        const meetups = [];
+        for (let key in data) {
+          let meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading && setLoadedMeetups) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  // Only return meetup data if not loading
   return (
     <section>
-      <h1>All Meetups</h1>      
-      <MeetupList meetups={DUMMY_DATA} />
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 }
